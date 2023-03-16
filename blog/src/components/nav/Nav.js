f7d2a5eab1a1c31f1c37d3a6506aa3ee77051ae2
d-slezak <96165933+d-slezak@users.noreply.github.com>
2023-03-16 23:50:02 +0000

    add colour to nav

--- blog/src/components/nav/Nav.js
+++ blog/src/components/nav/Nav.js
@@ -21,13 +21,10 @@ import Link from "next/link";
 
 export default function Nav() {
     return (
-        <header className="bg-gray-50">
+        <header className="bg-primary">
             <div className="xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3">
-                <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0">
-                    <input type="text" className="input-text" placeholder="Search"></input>
-                </div>
                 <div className="shrink w-80 sm:order-2">
-                    <Link href={"/"} className="font-bold uppercase text-3xl">
+                    <Link href={"/"} className="font-bold uppercase text-3xl hover:text-primary-accent ">
                         Dan.Slezak
                     </Link>
                 </div>
