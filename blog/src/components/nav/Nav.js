303afcefd09b0ff9f039d9630a97b177b2937480
d-slezak <96165933+d-slezak@users.noreply.github.com>
2023-03-21 15:01:23 +0000

    add navItems

--- blog/src/components/nav/Nav.js
+++ blog/src/components/nav/Nav.js
@@ -1,32 +1,44 @@
+import Link from "next/link";
+
+import NavItem from "./NavItem";
+
+import { ImFacebook, ImInstagram, ImTwitter } from "react-icons/im";
+
+
 let navItems = [{
-    Name: 'About Me',
+    title: 'About Me',
     link: '#'
 },
 {
-    Name: 'Blog',
+    title: 'Blog',
     link: '#'
 },
 {
-    Name: 'Projects',
+    title: 'Projects',
     link: '#'
 },
 {
-    Name: 'Contact',
+    title: 'Contact',
     link: '#'
 },
 ]
-
-import { ImFacebook, ImInstagram, ImTwitter } from "react-icons/im";
-import Link from "next/link";
 
 export default function Nav() {
     return (
         <header className="bg-primary">
             <div className="xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3">
-                <div className="shrink w-80 sm:order-2">
+                <div className="shrink w-80 sm:order-1">
                     <Link href={"/"} className="font-bold uppercase text-3xl hover:text-primary-accent ">
                         Dan.Slezak
                     </Link>
+                </div>
+                <div className="flex gap-10 order-2">
+                    
+                    {navItems.map(item => (
+                        <NavItem
+                            title = {item.title}
+                        />
+                    ))}
                 </div>
                 <div className="w-96 order-3 flex justify-center">
                     <div className="flex gap-6">
---
+++ blog/src/components/nav/NavItem.js
@@ -0,0 +1,7 @@
+export default function NavItem({title}) {
+    return (
+        <div className=" hover:text-primary-accent hover:underline">
+            {title}
+        </div>
+    )
+}
