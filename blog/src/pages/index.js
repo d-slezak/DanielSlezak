c2fffc53cb543cd8761510cc9fc2ba83dd1f40d6
d-slezak <96165933+d-slezak@users.noreply.github.com>
2023-03-22 01:03:25 +0000

    add landing page

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,10 +1,12 @@
 
 import Layout from '../layout/Layout'
+import Landing from './landing/Landing'
 
 export default function Home() {
   return (
     <div>
       <Layout>
+        <Landing/>
       </Layout>
 
     </div>
---
+++ blog/src/pages/landing/Landing.js
@@ -0,0 +1,9 @@
+export default function Landing() {
+    return (
+    <div className="flex justify-center">
+        <div className="flex flex-col">
+            <div>Hi, I'm Dan Slezak</div>
+        </div>
+    </div>
+    )
+}
