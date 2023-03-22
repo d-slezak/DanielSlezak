7d45df3293e46a39b08faadc8b4974d923341d2e
d-slezak <96165933+d-slezak@users.noreply.github.com>
2023-03-22 01:17:56 +0000

    started working on landing

--- blog/src/pages/landing/Landing.js
+++ blog/src/pages/landing/Landing.js
@@ -1,9 +1,8 @@
 export default function Landing() {
     return (
-    <div className="flex justify-center">
-        <div className="flex flex-col">
-            <div>Hi, I'm Dan Slezak</div>
-        </div>
+    <div className="flex flex-col justify-center flex-1 self-center ">
+        <div>Hi, I'm Dan Slezak</div>
+        <div>I am a Junior Developer working in Ontario, Canada</div>
     </div>
     )
 }
