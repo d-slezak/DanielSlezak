4d7db8734e3f08c95245595e7e73e47aaa79486d
d-slezak <96165933+d-slezak@users.noreply.github.com>
2023-03-22 01:26:00 +0000

    styling landing

--- blog/src/pages/landing/Landing.js
+++ blog/src/pages/landing/Landing.js
@@ -1,8 +1,13 @@
+import Image from "next/image"
+
 export default function Landing() {
     return (
-    <div className="flex flex-col justify-center flex-1 self-center ">
-        <div>Hi, I'm Dan Slezak</div>
-        <div>I am a Junior Developer working in Ontario, Canada</div>
+    <div className="flex flex-col gap-4 justify-center content-center flex-1 self-center ">
+        <div className="text-6xl text-secondary-light">Hi, I'm Dan Slezak</div>
+        <div className="image flex justify-center">
+                <Image src={"/images/img1.jpg"} width={400} height={400} />
+            </div>
+        <div className="text-lg">I am a <span className="text-secondary-accent font-extrabold">Junior Developer</span> working in Ontario, Canada</div>
     </div>
     )
 }
