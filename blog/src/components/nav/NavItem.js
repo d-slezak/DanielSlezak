53d99fb01827a87ee86e7493e429158ba6c870dc
d-slezak <96165933+d-slezak@users.noreply.github.com>
2023-04-09 18:30:54 +0000

    add color to active navItem

--- blog/src/components/nav/NavItem.js
+++ blog/src/components/nav/NavItem.js
@@ -1,9 +1,15 @@
 import Link from "next/link"
 
+import { useRouter } from "next/router"
+
 export default function NavItem({title, link}) {
+
+    const router = useRouter();
+    const currentRoute = router.pathname;
+
     return (
         <div className="text-lg hover:text-primary-accent hover:underline">
-            <Link href={(link ? link : '/')}>{title}</Link>
+            <Link href={(link ? link : '/')} className={currentRoute === link ? "font-extrabold text-secondary-accent underline" : null}>{title}</Link>
         </div>
     )
 }
