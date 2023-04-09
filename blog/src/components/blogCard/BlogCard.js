eb6a918f6201692d54faf3d0463e2b664d7b85df
d-slezak <96165933+d-slezak@users.noreply.github.com>
2023-04-09 20:21:08 +0000

    add blog hero section and starter sections for projects and newsletter

---
+++ blog/src/components/blogCard/BlogCard.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image"
+import { ImPriceTags } from "react-icons/im";
+
+export default function BlogCard ({title, description, datePosted, tags, thumbnail, link}) {
+    return (
+        <div className="w-4/6">
+            <div className="relative z-50 -ml-8 mr-8 -mb-8  bg-secondary-accent p-3 text-3xl border-4 border-primary rounded-md">{title}</div>
+            <div className="z-0 flex flex-col bg-white border-4 border-primary rounded-md p-4 pt-8">
+                <div className="text-secondary-dark text-xl">{datePosted}</div>
+                <div className="flex px-4 mb-4">
+                    <Image className="border-4 border-primary rounded-md w-4/5" src={thumbnail} width={400} height={400} />
+                    <div >
+                        <div className="flex gap-2 justify-center">
+                            <div className="flex flex-col justify-center"><ImPriceTags/> </div>
+                            <div>Tags</div>
+                        </div>
+                        {tags.map(tag => (
+                            <div className="border-2 rounded-md border-secondary-dark hover:border-secondary-accent bg-primary-accent hover:bg-secondary-dark hover:text-secondary-accent m-3 text-center">{tag}</div>
+                        ))}
+                    </div>
+                </div>
+                <div className="italic">{description}</div>
+            </div>
+
+        </div>
+    )
+}
--- blog/src/pages/landing/Landing.js
+++ blog/src/pages/landing/Landing.js
@@ -1,19 +1,52 @@
 import Image from "next/image"
+import BlogCard from "@/components/blogCard/BlogCard"
 
 export default function Landing() {
     return (
-    <div className="flex flex-col gap-4 justify-center content-center flex-1 self-center ">
         <div>
-            <div className="text-6xl text-secondary-light text-center">Hi, I'm Dan Slezak</div>
-            <div className="image flex justify-center">
-                    <Image className="rounded-full border-8 border-secondary-light my-4 " src={"/images/profile.png"} width={400} height={400} />
+            <div className="flex flex-col flex-1 self-center py-10">
+                <div>
+                    <div className="text-6xl text-secondary-light text-center">Hi, I'm Dan Slezak</div>
+                    <div className="image flex justify-center">
+                            <Image className="rounded-full border-8 border-secondary-light my-4 " src={"/images/profile.png"} width={400} height={400} />
+                        </div>
+                    <div className="text-lg text-center">I am a <span className="text-secondary-accent font-extrabold">Junior Developer</span> working in Ontario, Canada</div>
+                    <div className="text-lg text-center">Welcome to my Personal Website</div>
+                    <div className="flex content-center justify-center">
+                        <button className="bg-secondary-accent mt-8 px-8 py-4 border-4 rounded-3xl border-secondary-light text-white font-bold hover:border-primary"> About Me</button>
+                    </div>
                 </div>
-            <div className="text-lg text-center">I am a <span className="text-secondary-accent font-extrabold">Junior Developer</span> working in Ontario, Canada</div>
-            <div className="text-lg text-center">Welcome to my Personal Website</div>
-            <div className="flex content-center justify-center">
-                <button className="bg-secondary-accent mt-8 px-8 py-4 border-4 rounded-3xl border-secondary-light text-white font-bold hover:border-primary"> About Me</button>
+            </div>
+            <div className="bg-primary-accent flex justify-center py-10">
+                <div className="flex flex-col justify-center w-3/6">
+                    <div className="flex flex-col items-center justify-center pb-8 gap-2">
+                        <div className="text-center text-4xl font-extrabold"> Check Out</div>
+                        <button className="bg-secondary-accent w-1/5 px-8 py-4 rounded-3xl border-4 border-secondary-light text-white font-bold hover:border-primary">My Blog</button>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <div className="text-center text-2xl m-4"> Here's my latest post!</div>
+                        <BlogCard
+                            title={"The End of Front-End Development - Josh Comeau"}
+                            description={"Large language models like GPT-4 are becoming increasingly capable, at an alarming rate. Within a couple of years, we won't need developers any more! …Or at least, that's the narrative going viral on Twitter. I'm much more optimistic about what these AI advancements mean for the future of software development."}
+                            datePosted={"March 15th, 2023"}
+                            tags={["Software", "Front-End Developer"]}
+                            thumbnail={"/images/dummyBlogThumbNail.png"}
+                        ></BlogCard>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col py-10">
+                <div>
+                    Here are some of my work
+                </div>
+                <div>Project tiles</div>
+                <button>All Projects</button>
+            </div>
+            <div className="flex flex-col py-10 bg-primary-accent">
+                <div> Sign up for my newslettter </div>
+                <div>News Letter Card</div>
             </div>
         </div>
-    </div>
+
     )
 }
