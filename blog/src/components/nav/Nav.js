17ff20fcd658e403ee3d6e0f5e6e0d91a5636ae1
d-slezak <96165933+d-slezak@users.noreply.github.com>
2023-05-05 18:30:32 +0000

    Make navbar responsive

--- blog/src/components/nav/Nav.js
+++ blog/src/components/nav/Nav.js
@@ -24,14 +24,16 @@ let navItems = [{
 export default function Nav() {
     return (
         <header className="bg-primary">
-            <div className="xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3 text-primary-accent">
-                <div className="shrink w-80 sm:order-1">
+            <div className="xl:container xl:mx-auto flex items-center md:flex-row md:justify-between text-center py-3 text-primary-accent">
+                <div className="shrink md:w-80 order-2 md:order-1 fixed w-full md:relative">
                     <Link href={"/"} className="font-bold uppercase text-3xl hover:text-secondary-accent ">
                         Dan.Slezak
                     </Link>
                 </div>
-                <div className="flex gap-10 sm:order-2 px-6 pt-5 sm:pt-0">
-                    
+                <div className="order-1 self-start px-10 md:hidden">
+                    X
+                </div>
+                <div className="hidden sm:visible md:flex md:gap-10 md:px-6 md:pt-5 md:order-2">
                     {navItems.map(item => (
                         <NavItem
                             title = {item.title}
