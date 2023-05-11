a2973c991573cb48b5367ac366836ae78bf32675
d-slezak <96165933+d-slezak@users.noreply.github.com>
2023-05-11 17:39:58 +0000

    Add dropdown Icon

--- docs/src/components/nav/Nav.js
+++ docs/src/components/nav/Nav.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { useState } from "react";
 
+import { ImMenu3, ImMenu4 } from "react-icons/im";
+
 import NavItem from "./NavItem";
 
 
@@ -34,13 +36,13 @@ export default function Nav() {
     return (
         <header className="bg-primary">
             <div className="xl:container xl:mx-auto flex items-center md:flex-row md:justify-between text-center py-3 text-primary-accent">
-                <div className="shrink md:w-80 order-2 md:order-1 fixed w-full md:relative">
+                <div className="shrink md:w-80 order-2 md:order-1 absolute w-full md:relative">
                     <Link href={"/"} className="font-bold uppercase text-3xl hover:text-secondary-accent ">
                         Dan.Slezak
                     </Link>
                 </div>
                 <div className="order-1 self-start px-5 sm:px-10 z-10 md:hidden">
-                    <button className="btn " onClick={(e) => handleMenuClick()}>X</button>
+                    <button className="btn " onClick={(e) => handleMenuClick()}>{showMenu ? (<ImMenu4/>) : (<ImMenu3/>)}</button>
                 </div>
                 <div className="hidden sm:visible md:flex md:gap-10 md:px-6 md:pt-5 md:order-2">
                     {navItems.map(item => (
